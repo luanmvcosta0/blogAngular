8fc46ade474bc8f4904ac2fa478d40f384c884f4
luanmvcosta0 <luanmvcosta@gmail.com>
2025-02-14 13:11:24 -0300

    Finalizando o projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentComponent } from './pages/content/content.component';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { ContentComponent } from '../pages/content/content.component';
 
 
 const routes: Routes = [
